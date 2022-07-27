a6916dd3f6de1eedab71fe982bfd71cad7a3142e
Qwerty (Vítězslav Ackermann Ferko) <qwerty@qwerty.xyz>
2022-07-27 03:58:31 +0200

    Add ambient helper typings

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -20,3 +20,40 @@ interface MockContract<T extends BaseContract = BaseContract> extends _MockContr
     [key in keyof T['functions']]: Stub
   }
 }
+
+
+type UTCString = `${number}-${number}-${number} ${number}:${number}:${number} UTC` // e.g. 2022-06-01 16:47:55 UTC
+
+type AnyObject<T = any> = Record<string, T>
+type AnyFunction = (...args: any[]) => any
+
+type AllowArray<T> = T | T[]
+
+type ValuesOf<T> = T[keyof T]
+
+type Modify<T, R> = Omit<T, keyof R> & R
+
+type AllOrNone<T> = T | { [K in keyof T]?: never }
+
+// Window and library interfaces overrides -------------------------------------------------------------------------------------
+
+interface Window {
+  ethereum: EthereumProvider
+  web3: any
+  d: any // debug object, put anything here without TypeScript complaining about "Property 'd' does not exist on Window."
+}
+
+
+// ExternalProvider is the official type for the window.ethereum object, however, `new Web3(ethereum)` does not like it so we must improvise. https://github.com/MetaMask/providers
+declare type ExternalProvider = import('@ethersproject/providers').ExternalProvider
+declare type AbstractProvider = import('web3/node_modules/web3-core/types').AbstractProvider
+interface EthereumProvider extends ExternalProvider {
+  _state: {
+    accounts: string[]
+  }
+  /** web3-react/injected-connector listens to these events and re-emits them as its own type `ConnectorEvent`.*/
+  on(event: 'close' | 'accountsChanged' | 'chainChanged' | 'networkChanged', callback: (payload: any) => void): void
+  once(event: 'close' | 'accountsChanged' | 'chainChanged' | 'networkChanged', callback: (payload: any) => void): void
+  removeAllListeners(): void
+  sendAsync: AbstractProvider['sendAsync']
+}
