0ab07e6da5decb53870d0b7f7812af78a1de8641
Qwerty (Vítězslav Ackermann Ferko) <qwerty@qwerty.xyz>
2024-08-29 12:22:27 +0200

    Implement custom cursors

--- src/components/Organisms/index.ts
+++ src/components/Organisms/index.ts
@@ -1,1 +1,2 @@
+export { Cursors } from './Cursors'
 export { Navigation } from './Navigation'
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
-import '@/styles/globals.css'
-import { useLayoutEffect } from 'react'
-import type { AppProps } from 'next/app'
-import { Inter } from 'next/font/google'
-import Head from 'next/head'
+import { useLayoutEffect } from "react"
+import type { AppProps } from "next/app"
+import { Inter } from "next/font/google"
+import Head from "next/head"
 
-import { Navigation } from '@/components'
+import { Cursors, Navigation } from "@/components"
+import "@/styles/globals.css"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export default function App({ Component, pageProps }: AppProps) {
+
   useLayoutEffect(() => {
-    document.getElementById('RootElement')?.classList.add('bg' + (((Math.random() * 7) | 0) + 1))
+    document.getElementById("RootElement")?.classList.add("bg" + (((Math.random() * 7) | 0) + 1))
   }, [])
 
   return (
@@ -21,6 +22,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel='icon' href='/favicon.png' />
       </Head>
       <Navigation />
+
+      <Cursors />
+
       <Component {...pageProps} />
     </main>
   )
