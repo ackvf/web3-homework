c006043660444c8d8b7693f7878f72d8688181da
Qwerty (Vítězslav Ackermann Ferko) <qwerty@qwerty.xyz>
2025-02-07 20:53:59 +0100

    Add wagmi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import { useLayoutEffect } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import type { AppProps } from "next/app"
 import { Inter } from "next/font/google"
 import Head from "next/head"
+import { WagmiProvider } from "wagmi"
 
-import { Cursor, Navigation } from "@/components"
 import "@/styles/globals.css"
+import { config } from "@/web3.config"
+import { Cursor, Navigation } from "@/components"
 
 const inter = Inter({ subsets: ["latin"] })
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -16,18 +21,20 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [])
 
   return (
-    <>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
       <Cursor />
-      <main id='RootElement' className={`${inter.className} flex h-screen flex-col pt-28`}>
+      <main id="RootElement" className={`${inter.className} flex h-screen flex-col pt-28`}>
         <Head>
           <title>Wallet by Qwerty</title>
-          <meta name='description' content='Wallet by Qwerty.' />
-          <link rel='icon' href='/favicon.png' />
+          <meta name="description" content="Wallet by Qwerty." />
+          <link rel="icon" href="/favicon.png" />
         </Head>
         <Navigation />
         <Component {...pageProps} />
-      </main >
-    </>
+        </main >
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 
 }
